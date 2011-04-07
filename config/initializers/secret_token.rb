096e1f85d3e8ebef0edafa8c9f2b5b8486af03ea
José Mota <josemota.net@gmail.com>
2011-04-07 20:35:28 +0100

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubynorte::Application.config.secret_token = 'bbd47d139a591487685bb5b665b9b5c6a220ed8938170105b7fde55c8bafd446d354c01888b6477d334fc03d67d5426fb60010df79e1d70e3496637afe31a2c4'

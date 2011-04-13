4ffea32cf1a0cbb13a43d309f87b0d9d681d53c6
José Mota <josemota.net@gmail.com>
2011-04-13 19:30:07 +0100

    Twitter credentials.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :twitter, '', ''
+  provider :twitter, 'dcYQRkOe3fhZFAFC6xfg', 'i5wf0j0eU02bi2oRIPzp8lDZ4JaCdIiaw6VtyuY'
 end

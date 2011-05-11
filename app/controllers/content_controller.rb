f21368a4fd9440f69abe25a7ccc04b8f2d45fe74
José Mota <josemota.net@gmail.com>
2011-05-11 20:20:01 +0100

    Sort admissions by arrival. Update CSS.

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -2,7 +2,7 @@ class ContentController < ApplicationController
   def index
     @talks = Talk.accepted
     @talk  = Talk.new
-    @admissions = Admission.all
+    @admissions = Admission.order('created_at')
 
     @user = Admission.find_by_provider_and_uid session[:user][:provider], session[:user][:uid] if session[:user]
   end

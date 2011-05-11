d32ebe00aa8b2c3b758978c599d780747bd01308
José Mota <josemota.net@gmail.com>
2011-05-11 20:08:12 +0100

    Fix bad params sent to update admission.

--- app/controllers/admissions_controller.rb
+++ app/controllers/admissions_controller.rb
@@ -23,7 +23,7 @@ class AdmissionsController < ApplicationController
 
   def update
     @admission = Admission.find_by_provider_and_uid session[:user][:provider], session[:user][:uid]
-    if @admission.update_attributes params[:user]
+    if @admission.update_attributes params[:admission]
       session[:user][:confirmed] = true
       redirect_to root_url, :notice => "Obrigado por teres confirmado que vens. Ficamos felizes por vires, até lá!"
     end

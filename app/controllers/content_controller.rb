d9eaadca159f210a3063d304692178e0f234cf10
José Mota <josemota.net@gmail.com>
2011-05-18 23:03:23 +0100

    Sort admissions back to creation date ... again.

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -2,7 +2,7 @@ class ContentController < ApplicationController
   def index
     @talks = Talk.accepted
     @talk  = Talk.new
-    @admissions = Admission.order('name')
+    @admissions = Admission.order('created_at')
     @confirmed_count = @admissions.map { |ab| ab.confirmed }.count { |ac| ac == true }
 
     @user = Admission.find_by_provider_and_uid session[:user][:provider], session[:user][:uid] if session[:user]
